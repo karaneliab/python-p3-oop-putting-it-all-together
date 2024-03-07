dfbb472cc9aecd557fbf5d36564c3a29fb846335
karaneliab <eliabkaran7884@gmail.com>
2024-03-07 21:35:31 +0300

    not passing

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self.title = title
+        self.page_count = page_count
+        # self.turn_page = turn_page
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+
+  
+    def turn_page(self):       
+        print("Flipping the page...wow, you read fast!")
+    
+    
+    
+        
+
+    
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self,brand ,size):
+        self.brand = brand
+        self.size = size
+        self.condition = 'New'
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self,value):
+        if isinstance(value , int):
+            self._size = value
+        else:
+            print("size must be an integer")
+    def cobble(self):
+        print("The shoe has been repaired")
+        self.condition = 'New'  
+
+
+shoe = Shoe('Nike', 9)
+print(f'Condition before repair: {shoe.condition}')
+shoe.cobble()
+print(f'Condition after repair: {shoe.condition}')
